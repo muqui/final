56ec731a1897e9465108eb41b6f82768b4b7b1cc
Juan Francisco Bue <buejuanfrancisco@gmail.com>
2025-02-07 13:51:37 -0300

    Users Service and Users Repository

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './User.entity';
+import { UsersService } from './users.service';
+import { UsersRepository } from './users.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
+  providers: [UsersService, UsersRepository],
 })
 export class UsersModule {}
