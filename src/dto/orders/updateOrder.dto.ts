af426a70461f72de2e45c5c027230393d7a1f75d
OAPadillaHerrera <oscar.padilla.ha@gmail.com>
2025-02-09 10:59:17 -0500

    Guardando cambios antes de cambiar de rama

--- src/dto/orders/updateOrder.dto.ts
+++ src/dto/orders/updateOrder.dto.ts
@@ -1,12 +1,16 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateOrderDto } from './createOrder.dto';
-import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { IsBoolean, IsEnum, IsOptional, IsString } from 'class-validator';
 import { OrderStatus } from 'src/enum/orderstatus.enum';
 
 export class UpdateOrderDto extends PartialType(CreateOrderDto) {
   @IsOptional()
   @IsEnum(OrderStatus)
   status?: OrderStatus;
+
+  @IsOptional()
+  @IsBoolean()
+  isActive?: boolean;
 
   @IsOptional()
   @IsString()
