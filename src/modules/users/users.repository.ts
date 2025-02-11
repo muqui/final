56591a6fd6a46124277502c189d586c8d809652b
Juan Francisco Bue <buejuanfrancisco@gmail.com>
2025-02-10 23:46:26 -0300

    Borrando comentarios en users.repository

--- src/modules/users/users.repository.ts
+++ src/modules/users/users.repository.ts
@@ -1,13 +1,3 @@
-/*import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { User } from './User.entity';
-import { Repository } from 'typeorm';
-
-@Injectable()
-export class UsersRepository {
-  constructor(@InjectRepository(User) users: Repository<User>) {}
-}*/
-
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './User.entity';
@@ -15,9 +5,11 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersRepository {
-  constructor(@InjectRepository(User) private readonly usersRepository: Repository<User>) {}
+  constructor(
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
+  ) {}
 
   async findUserById(id: string): Promise<User | null> {
-    return this.usersRepository.findOne({ where: { id } }); 
+    return this.usersRepository.findOne({ where: { id } });
   }
 }
