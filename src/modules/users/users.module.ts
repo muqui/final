111a5d042f370a61c0931f3205200bc8b0aa92b8
Juan Francisco Bue <buejuanfrancisco@gmail.com>
2025-02-10 23:52:49 -0300

    Agregando el exports en UsersModule

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -4,13 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './User.entity';
 import { UsersService } from './users.service';
 import { UsersRepository } from './users.repository';
-import { OrdersRepository } from '../orders/orders.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
 
-  exports: [OrdersRepository],
+  exports: [UsersRepository],
 })
 export class UsersModule {}
